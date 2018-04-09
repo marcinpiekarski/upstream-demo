fbad2a9ac9d4448ef7bbc4b7f416dc86a15f6bf5
Marcin Piekarski <marcin.piekarski@comarch.pl>
2018-04-09 16:37:20 +0200

    sdfsd

--- src/test3/test.js
+++ src/test3/test.js
@@ -5,3 +5,4 @@ console.log('test-merge 2');asdasdaaaaaaaaa     dsdfsdfasdfsdf
 console.log('test-merge 2');asdasdaaaaaaaaa     dsdfsdfasdfsdf
 console.log('test-merge 2');asdasdaaaaaaaaa     dsdfsdfasdfsdf
 console.log('test-merge 2');asdasdaaaaaaaaa     dsdfsdfasdfsdf
+console.log('test-merge 2');asdasdaaaaaaaaa     dsdfsdfasdfsdf
